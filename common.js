c0984da72d038c5370fde80d1db0b89c48bbb894
gavinmorrow <gavinfmorrow@gmail.com>
2024-09-03 09:24:36 -0400

    clean up

--- common.js
+++ common.js
@@ -1,12 +1,14 @@
+let DEFAULT_TIMEOUT = 10000;
+
 /**
  * Waits for a function to return a truthy value.
  * @template T The type of the value to wait for.
  * @param {() => T} fn A function that returns the value to wait for. If the value is truthy, the promise resolves.
- * @param {number|undefined} timeout The amount to wait before failing (in ms). If undefined, will wait indefinitely. Defaults to undefined.
+ * @param {number|undefined} timeout The amount to wait before failing (in ms). If undefined, will wait indefinitely. Defaults to DEFAULT_TIMEOUT.
  * @param {number} interval The interval to check for the element (in ms). Defaults to 100.
- * @returns {T|null} The value returned by the function, or null if the timeout is reached.
+ * @returns {Promise<T|null>} The value returned by the function, or rejects with null if the timeout is reached.
  */
-const waitFor = (fn, timeout = undefined, interval = 100) =>
+const waitFor = (fn, timeout = DEFAULT_TIMEOUT, interval = 100) =>
   new Promise((resolve, reject) => {
     let timeoutId =
       timeout != undefined ? setTimeout(() => reject(null), timeout) : null;
@@ -23,11 +25,11 @@ const waitFor = (fn, timeout = undefined, interval = 100) =>
 /**
  * Waits for an element to appear on the page.
  * @param {String} selector The query selector to wait for.
- * @param {number|undefined} timeout The amount to wait before failing (in ms). If undefined, will wait indefinitely. Defaults to undefined.
+ * @param {number|undefined} timeout The amount to wait before failing (in ms). If undefined, will wait indefinitely. Defaults to DEFAULT_TIMEOUT.
  * @param {number} interval The interval to check for the element (in ms). Defaults to 100.
- * @returns {Promise<HTMLElement|null>} A promise that resolves to the element if it is found, or null if the timeout is reached.
+ * @returns {Promise<HTMLElement|null>} A promise that resolves to the element if it is found, or rejects with null if the timeout is reached.
  */
-const waitForElem = (selector, timeout = undefined, interval = 100) =>
+const waitForElem = (selector, timeout = DEFAULT_TIMEOUT, interval = 100) =>
   waitFor(() => document.querySelector(selector), timeout, interval);
 
 /**
@@ -35,5 +37,17 @@ const waitForElem = (selector, timeout = undefined, interval = 100) =>
  * @param {HTMLElement} elem
  */
 const clickElem = (elem) => elem.click();
+
+/**
+ * Curries a function to click an element once a condition has been reached.
+ * @param {() => boolean} condFn
+ * @param {number|undefined} timeout The amount to wait before failing (in ms). If undefined, will wait indefinitely. Defaults to DEFAULT_TIMEOUT.
+ * @param {number} interval A promise that resolves to the element if it is found, or null if the timeout is reached.
+ * @returns {(HTMLElement) => Promise<void|null>} A function that takes an element and clicks it once `condFn` has been reached.
+ */
+const clickWhen =
+  (condFn, timeout = DEFAULT_TIMEOUT, interval = 100) =>
+  (elem) =>
+    waitFor(condFn, timeout, interval).then((_) => elem.click());
 
 console.log("Ready!");
--- login-automatically.js
+++ login-automatically.js
@@ -3,31 +3,32 @@
 
 console.log("Logging in automatically...");
 
-const DEFAULT_TIMEOUT = 10000; // 10 seconds
-
 // don't use await, so that it works if started part way through the login process
 
-// hunter login page
-waitForElem("input[type='submit']#nextBtn", DEFAULT_TIMEOUT)
-  .then((btn) =>
-    waitFor(() => document.getElementById("Username").value != "").then((_) =>
-      btn.click(),
-    ),
-  )
-  .catch((_) => console.error("Failed to find hunter login button"));
+{
+  // hunter login page
+  const nextButton = "input[type='submit']#nextBtn";
+  const usernameEntered = () => document.getElementById("Username").value != "";
+  waitForElem(nextButton)
+    .then(clickWhen(usernameEntered))
+    .catch((_) => console.error("Failed to find hunter login button"));
+}
 
-// google login page
-// find first hunter email
-waitForElem("[data-identifier$='@hunterschools.org']", DEFAULT_TIMEOUT)
-  .then(clickElem)
-  .catch((_) => console.error("Failed to find google email."));
-// wait for password
-// this works best w/ password autofill
-waitFor(
-  () =>
+{
+  // google login page
+  // find first hunter email
+  const hunterEmail = "[data-identifier$='@hunterschools.org']";
+  waitForElem(hunterEmail)
+    .then(clickElem)
+    .catch((_) => console.error("Failed to find google email."));
+
+  // wait for password
+  // this works best w/ password autofill
+  const passwordNextBtn = () =>
     Array.from(document.querySelectorAll("button span")).filter((e) => {
       return e.textContent === "Next";
-    })[0],
-)
-  .then(clickElem)
-  .catch((_) => console.error("Could not find password next button."));
+    })[0];
+  waitFor(passwordNextBtn)
+    .then(clickElem)
+    .catch((_) => console.error("Could not find password next button."));
+}
