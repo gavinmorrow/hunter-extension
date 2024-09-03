866b8ebe4d3b2a7725ca873b2bf6d21021762e15
gavinmorrow <gavinfmorrow@gmail.com>
2024-09-03 11:09:15 -0400

    update style of assignment-center.js to match

--- assignment-center.js
+++ assignment-center.js
@@ -1,5 +1,13 @@
-/// FIXES: At the top of the calendar, if the month is too long, it wraps onto
-/// the next line. However, the containing div doesn't grow to fit.
-waitForElem("#calendar-date-container").then(
-  (elem) => (elem.style.height = "fit-content"),
-);
+console.log("Modifying assignment center...");
+
+/**
+ * FIXES: At the top of the calendar, if the month is too long, it wraps onto
+ * the next line. However, the containing div doesn't grow to fit.
+ */
+const fixCalendarHeaderOverflow = async () => {
+  const calHeader = await waitForElem("#calendar-date-container");
+  if (calHeader) calHeader.style.height = "fit-content";
+  else console.error("calendar header not found.");
+};
+
+Promise.allSettled([fixCalendarHeaderOverflow()]);
