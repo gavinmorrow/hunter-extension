8c5c726cf6da6c839e9e73e31172ca508bea9412
gavinmorrow <gavinfmorrow@gmail.com>
2024-09-03 07:52:57 -0400

    add jsdoc type annotations

--- fix-month-wrap.js
+++ fix-month-wrap.js
@@ -1,5 +1,6 @@
 /// FIXES: At the top of the calendar, if the month is too long, it wraps onto
 /// the next line. However, the containing div doesn't grow to fit.
 waitForElem("#calendar-date-container").then(
+  /** @param {HTMLDivElement} elem */
   (elem) => (elem.style.height = "fit-content"),
 );
