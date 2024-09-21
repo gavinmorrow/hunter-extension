fa57156fe1d701c33b05994961dab043233d3e26
gavinmorrow <gavinfmorrow@gmail.com>
2024-09-21 09:32:56 -0400

    Add assignment scraping

--- assignment-center.js
+++ assignment-center.js
@@ -69,6 +69,119 @@ const filterByNotCompleted = featureFlag(
   },
 );
 
+/**
+ * @typedef {String} Color A CSS color in `rgb(<r>,<g>,<b>)` format
+ * @typedef {String} Link
+ */
+
+/**
+ * @typedef {Object} AssignmentDetails
+ * @property {String} dueDate
+ * @property {String} assignedDate
+ * @property {Number?} maxPoints
+ * @property {{ name: String, link: Link }?} class
+ * @property {String} type
+ * @property {boolean} isTask
+ */
+
+/**
+ * @typedef {Object} Assignment
+ * @property {Color} color
+ * @property {String} title
+ * @property {Link} link
+ * @property {AssignmentDetails} details
+ */
+
+/** @returns {Promise<Assignment>} */
+const scrapeAssignments = async () => {
+  // sky-split-view-workspace-content Whole section
+  //   app-student-assignments-repeater Each header(+content)
+  //     sky-repeater-item-content content For each header
+  //       sky-repeater sky-repeater-item-title Indiv assignment
+  //           div.left-block Assignment color (its in the style:background-color)
+  //           div.middle-block Assignment data
+  //             div:nth(0) Assignment title
+  //               app-assignment-title-link a The link itself
+  //             div.assignment-details Assignment details
+  //               text node The " Due: 9/20/2024 11:59 PM | Assigned: 9/12/2024 12:00 AM " text
+  //               span:nth(0) button a The class link "CHEMISTRY - 60"
+  //               span:nth(1) The " | Test/Exam (Major) "
+  //               span:nth(2) The " | Assignment "
+  //           div.right-block app-assignment-status-display Assignment status
+  //               sky-icon i::before Status icon
+  //               button Status text // FIXME: does this work when you can't click it?
+
+  // const SECTION_NAMES = [
+  //   "Missing or overdue",
+  //   "Due today",
+  //   "Due tomorrow",
+  //   "Due this week",
+  //   "Due next week",
+  //   "Due after next week",
+  // ];
+
+  /**
+   * @param {HTMLElement} detailsElem
+   * @returns {AssignmentDetails}
+   */
+  const parseFullDetailsElem = (detailsElem) => {
+    const details = detailsElem.textContent;
+    const parts = details.split("|").map((part) => part.trim());
+
+    // invalid length
+    if (parts.length !== 4 && parts.length !== 5 && parts.length !== 6) {
+      console.error(`Invalid parts length: ${parts.length}`);
+      return null;
+    }
+    // tasks cannot have points, so this is okay
+    const isTask = parts.length === 4;
+    const hasPoints = parts.length === 6;
+
+    // first two elements
+    const [dueDate, assignedDate] = parts;
+    const maxPoints = hasPoints ? parseInt(parts[2]) : null;
+    // *maybe* the third element
+    const class_ = isTask
+      ? null
+      : { name: parts[2], link: detailsElem.querySelector("button a").href };
+    // the last two elements (type is second-to-last).
+    // _assignmentOrTask is the literal string "Assignment" or "My tasks",
+    // which isn't very useful for anything, so it is ignored.
+    const [_assignmentOrTask, type] = parts.toReversed();
+
+    return {
+      dueDate,
+      assignedDate,
+      maxPoints,
+      class: class_,
+      type,
+      isTask,
+    };
+  };
+
+  const assignments = await waitForElems(
+    "app-student-assignments-repeater sky-repeater-item-content sky-repeater sky-repeater-item-title",
+  );
+  if (assignments == null) return null;
+
+  return Array.from(assignments).map((elem) => {
+    const color = elem.querySelector("div.left-block").style.backgroundColor;
+    const { textContent: title, href: link } = elem.querySelector(
+      "div.middle-block app-assignment-title-link a",
+    );
+    const details = parseFullDetailsElem(
+      elem.querySelector("div.middle-block div.assignment-details"),
+    );
+
+    return {
+      color,
+      title,
+      link,
+      details,
+    };
+  });
+};
+
 const statusColorFor = async (status) => {
   const {
     assignmentCenter: { statusColors },
