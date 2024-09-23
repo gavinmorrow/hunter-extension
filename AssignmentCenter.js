2eb2429d18d56100236dbf648e742cf807107d1c
gavinmorrow <gavinfmorrow@gmail.com>
2024-09-23 16:54:06 -0400

    Extract Calendar (date) manipulation to seperate file

--- AssignmentCenter.js
+++ AssignmentCenter.js
@@ -81,10 +81,10 @@ ol > li > ul {
     // MAKE SURE TO HANDLE DATES CORRECTLY!!
     // **Be careful when doing custom date manipulation.**
     const today = new Date();
-    const dateOfMonday = AssignmentCenter.#dateForMondayOfWeek(today);
+    const dateOfMonday = Calendar.dateForMondayOfWeek(today);
     Array(7 /* days */ * 4 /* weeks */)
       .fill(0)
-      .map((_, i) => AssignmentCenter.#offsetFromDay(dateOfMonday, i))
+      .map((_, i) => Calendar.offsetFromDay(dateOfMonday, i))
       .map((date) => {
         // filter out weekends
         if (date.getDay() == 0 || date.getDay() == 6) return null;
@@ -93,9 +93,7 @@ ol > li > ul {
         li.value = date.getDate();
         const list = document.createElement("ul");
         this.assignments
-          .filter((a) =>
-            AssignmentCenter.#datesAreSameDay(a.details.dueDate, date),
-          )
+          .filter((a) => Calendar.datesAreSameDay(a.details.dueDate, date))
           .map((a) => new AssignmentBox(a))
           .map((e) => {
             const li = document.createElement("li");
@@ -227,131 +225,9 @@ ol > li > ul {
     const [date, time, amPm] = str.split(" ");
     const [month, day, year] = date.split("/").map(Number);
     const [hour12, min] = time.split(":").map(Number);
-    const hour24 = AssignmentCenter.#hour12ToHour24(hour12, amPm);
+    const hour24 = Calendar.hour12ToHour24(hour12, amPm);
 
     return new Date(year, month - 1, day, hour24, min);
-  }
-
-  /**
-   * Convert a time from 12-hour time to 24-hour time.
-   * @param {Number} hour The 12-hour hour.
-   * @param {"AM"|"PM"} amPm
-   * @returns {Number}
-   */
-  static #hour12ToHour24(hour, amPm) {
-    switch (amPm) {
-      case "AM":
-        // 12AM, ie midnight, is 00:00
-        if (hour === 12) return 0;
-        return hour;
-      case "PM":
-        // 12PM, ie noon, is 12:00
-        if (hour === 12) return 12;
-        return hour + 12;
-    }
-  }
-
-  /** @param {Date} date @returns {Date} */
-  static #dateForMondayOfWeek(date) {
-    // MAKE SURE TO HANDLE DATES CORRECTLY!!
-    // **Be careful when doing custom date manipulation.**
-
-    /** number from 0-6 (sunday-saturday) */
-    const dayOfWeek = date.getDay();
-    /** how much to add to current date to get the monday in the week */
-    const offsetFromMonday = 1 - dayOfWeek;
-    return AssignmentCenter.#offsetFromDay(date, offsetFromMonday);
-  }
-
-  /**
-   * @param {Date} date
-   * @param {Number} offset a number in days
-   * @returns {Date}
-   */
-  static #offsetFromDay(date, offset) {
-    // MAKE SURE TO HANDLE DATES CORRECTLY!!
-    // **Be careful when doing custom date manipulation.**
-
-    const dayInMonth = date.getDate();
-    const dayWithOffset = dayInMonth + offset;
-
-    // See https://stackoverflow.com/a/315767
-    const numDaysInCurrentMonth = AssignmentCenter.#numDaysInMonth(
-      date.getMonth(),
-      date.getFullYear(),
-    );
-    if (dayWithOffset > numDaysInCurrentMonth) {
-      // roll over to next month
-
-      const remainingOffset = dayWithOffset - numDaysInCurrentMonth;
-
-      // handle rolling over years too
-      // remember that months are 0-indexed (so 0-11)
-      const newYear = date.getFullYear() + (date.getMonth() === 11 ? 1 : 0);
-      const newMonth = (date.getMonth() + 1) % 12;
-      const newDay = 1;
-      const newDate = new Date(newYear, newMonth, newDay);
-
-      // be recursive to handle rolling over multiple months
-      return AssignmentCenter.#offsetFromDay(newDate, remainingOffset);
-    } else if (dayWithOffset < 1) {
-      // roll over to previous month
-
-      // handle rolling over years too
-      // remember that months are 0-indexed (so 0-11)
-      const newYear = date.getFullYear() - (date.getMonth() === 0 ? 1 : 0);
-      // `%` in js isn't modulo, it's remainder. https://stackoverflow.com/a/4467559
-      // This does modulo, so eg -2 -> 10
-      const newMonth = (((date.getMonth() - 1) % 12) + 12) % 12;
-
-      const numDaysInPrevMonth = AssignmentCenter.#numDaysInMonth(
-        newMonth,
-        newYear,
-      );
-
-      const newDay = numDaysInPrevMonth;
-      /** the end on the month */
-      const newDate = new Date(newYear, newMonth, newDay);
-
-      // dayWithOffset is negative or 0
-      // this is fine because this is setting the newDate to be the end of the month
-      const remainingOffset = dayWithOffset;
-
-      // be recursive to handle rolling over multiple months
-      return AssignmentCenter.#offsetFromDay(newDate, remainingOffset);
-    } else {
-      // oh my god finally the day is within the current month
-      // 1 <= dayWithOffset <= numDaysInCurrentMonth
-      const year = date.getFullYear();
-      const month = date.getMonth();
-      const day = dayWithOffset;
-
-      return new Date(year, month, day);
-    }
-  }
-
-  /**
-   * Get the number of days in a given month.
-   * @param {Number} month from 0-11 (Jan-Dec)
-   * @param {Number} year defaults to the current year
-   * @link https://stackoverflow.com/a/315767
-   */
-  static #numDaysInMonth(month, year = new Date().getFullYear()) {
-    // setting the day to 0 gets the last day of the previous month
-    return new Date(year, month + 1, 0).getDate();
-  }
-
-  /**
-   * Checks if two dates are on the same day.
-   * @param {Date} a
-   * @param {Date} b
-   */
-  static #datesAreSameDay(a, b) {
-    // Erase the time component from both, then compare.
-    const aErased = new Date(a.getFullYear(), a.getMonth(), a.getDate());
-    const bErased = new Date(b.getFullYear(), b.getMonth(), b.getDate());
-
-    return aErased.getTime() === bErased.getTime();
   }
 }
 
---
+++ Calendar.util.js
@@ -0,0 +1,123 @@
+/**
+ * Safe wrapper around JS `Date` for some common util methods.
+ */
+const Calendar = {
+  /**
+   * @param {Date} date
+   * @param {Number} offset a number in days
+   * @returns {Date}
+   */
+  offsetFromDay(date, offset) {
+    // MAKE SURE TO HANDLE DATES CORRECTLY!!
+    // **Be careful when doing custom date manipulation.**
+
+    const dayInMonth = date.getDate();
+    const dayWithOffset = dayInMonth + offset;
+
+    // See https://stackoverflow.com/a/315767
+    const numDaysInCurrentMonth = Calendar.numDaysInMonth(
+      date.getMonth(),
+      date.getFullYear(),
+    );
+    if (dayWithOffset > numDaysInCurrentMonth) {
+      // roll over to next month
+
+      const remainingOffset = dayWithOffset - numDaysInCurrentMonth;
+
+      // handle rolling over years too
+      // remember that months are 0-indexed (so 0-11)
+      const newYear = date.getFullYear() + (date.getMonth() === 11 ? 1 : 0);
+      const newMonth = (date.getMonth() + 1) % 12;
+      const newDay = 1;
+      const newDate = new Date(newYear, newMonth, newDay);
+
+      // be recursive to handle rolling over multiple months
+      return Calendar.offsetFromDay(newDate, remainingOffset);
+    } else if (dayWithOffset < 1) {
+      // roll over to previous month
+
+      // handle rolling over years too
+      // remember that months are 0-indexed (so 0-11)
+      const newYear = date.getFullYear() - (date.getMonth() === 0 ? 1 : 0);
+      // `%` in js isn't modulo, it's remainder. https://stackoverflow.com/a/4467559
+      // This does modulo, so eg -2 -> 10
+      const newMonth = (((date.getMonth() - 1) % 12) + 12) % 12;
+
+      const numDaysInPrevMonth = Calendar.numDaysInMonth(newMonth, newYear);
+
+      const newDay = numDaysInPrevMonth;
+      /** the end on the month */
+      const newDate = new Date(newYear, newMonth, newDay);
+
+      // dayWithOffset is negative or 0
+      // this is fine because this is setting the newDate to be the end of the month
+      const remainingOffset = dayWithOffset;
+
+      // be recursive to handle rolling over multiple months
+      return Calendar.offsetFromDay(newDate, remainingOffset);
+    } else {
+      // oh my god finally the day is within the current month
+      // 1 <= dayWithOffset <= numDaysInCurrentMonth
+      const year = date.getFullYear();
+      const month = date.getMonth();
+      const day = dayWithOffset;
+
+      return new Date(year, month, day);
+    }
+  },
+
+  /**
+   * Get the number of days in a given month.
+   * @param {Number} month from 0-11 (Jan-Dec)
+   * @param {Number} year defaults to the current year
+   * @link https://stackoverflow.com/a/315767
+   */
+  numDaysInMonth(month, year = new Date().getFullYear()) {
+    // setting the day to 0 gets the last day of the previous month
+    return new Date(year, month + 1, 0).getDate();
+  },
+
+  /**
+   * Checks if two dates are on the same day.
+   * @param {Date} a
+   * @param {Date} b
+   */
+  datesAreSameDay(a, b) {
+    // Erase the time component from both, then compare.
+    const aErased = new Date(a.getFullYear(), a.getMonth(), a.getDate());
+    const bErased = new Date(b.getFullYear(), b.getMonth(), b.getDate());
+
+    return aErased.getTime() === bErased.getTime();
+  },
+
+  /**
+   * Convert a time from 12-hour time to 24-hour time.
+   * @param {Number} hour The 12-hour hour.
+   * @param {"AM"|"PM"} amPm
+   * @returns {Number}
+   */
+  hour12ToHour24(hour, amPm) {
+    switch (amPm) {
+      case "AM":
+        // 12AM, ie midnight, is 00:00
+        if (hour === 12) return 0;
+        return hour;
+      case "PM":
+        // 12PM, ie noon, is 12:00
+        if (hour === 12) return 12;
+        return hour + 12;
+    }
+  },
+
+  /** @param {Date} date @returns {Date} */
+  dateForMondayOfWeek(date) {
+    // MAKE SURE TO HANDLE DATES CORRECTLY!!
+    // **Be careful when doing custom date manipulation.**
+
+    /** number from 0-6 (sunday-saturday) */
+    const dayOfWeek = date.getDay();
+    /** how much to add to current date to get the monday in the week */
+    const offsetFromMonday = 1 - dayOfWeek;
+    return Calendar.offsetFromDay(date, offsetFromMonday);
+  },
+};
