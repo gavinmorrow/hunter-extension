7551ce365937ba504c2b1c0b551d9903fbd4c212
gavinmorrow <gavinfmorrow@gmail.com>
2024-09-23 06:49:56 -0400

    Create calendar view for assignments

--- AssignmentCenter.js
+++ AssignmentCenter.js
@@ -54,20 +54,50 @@ class AssignmentCenter extends HTMLElement {
 ol {
   display: grid;
   grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(4, 1fr);
+}
+ol > li {
+  background-color: lightgreen;
+  border: 2px solid red;
 }
 `;
     shadow.appendChild(style);
 
-    const text = document.createElement("ol");
-    text.append(
-      ...this.assignments.map((assignment) => {
+    shadow.appendChild(this.#createCalendarGrid());
+  }
+
+  #createCalendarGrid() {
+    const grid = document.createElement("ol");
+
+    // create 4 weeks starting from this week
+    // MAKE SURE TO HANDLE DATES CORRECTLY!!
+    // **Be careful when doing custom date manipulation.**
+    const today = new Date();
+    const dateOfMonday = AssignmentCenter.#dateForMondayOfWeek(today);
+    Array(5 /* days */ * 4 /* weeks */)
+      .fill(0)
+      .map((_, i) => AssignmentCenter.#offsetFromDay(dateOfMonday, i))
+      .map((date) => {
         const li = document.createElement("li");
-        const box = new AssignmentBox(assignment);
-        li.appendChild(box);
+        li.value = date.getDate();
+        const list = document.createElement("ul");
+        this.assignments
+          .filter((a) =>
+            AssignmentCenter.#datesAreSameDay(a.details.dueDate, date),
+          )
+          .map((a) => new AssignmentBox(a))
+          .map((e) => {
+            const li = document.createElement("li");
+            li.appendChild(e);
+            return li;
+          })
+          .forEach((li) => list.appendChild(li));
+        li.appendChild(list);
         return li;
-      }),
-    );
-    shadow.appendChild(text);
+      })
+      .forEach((list) => grid.appendChild(list));
+
+    return grid;
   }
 
   /** @returns {Promise<Assignment[]>} A promise of an array of Assignments sorted by due date. */
@@ -207,6 +237,109 @@ ol {
         if (hour === 12) return 12;
         return hour + 12;
     }
+  }
+
+  /** @param {Date} date @returns {Date} */
+  static #dateForMondayOfWeek(date) {
+    // MAKE SURE TO HANDLE DATES CORRECTLY!!
+    // **Be careful when doing custom date manipulation.**
+
+    /** number from 0-6 (sunday-saturday) */
+    const dayOfWeek = date.getDay();
+    /** how much to add to current date to get the monday in the week */
+    const offsetFromMonday = 1 - dayOfWeek;
+    return AssignmentCenter.#offsetFromDay(date, offsetFromMonday);
+  }
+
+  /**
+   * @param {Date} date
+   * @param {Number} offset a number in days
+   * @returns {Date}
+   */
+  static #offsetFromDay(date, offset) {
+    // MAKE SURE TO HANDLE DATES CORRECTLY!!
+    // **Be careful when doing custom date manipulation.**
+
+    const dayInMonth = date.getDate();
+    const dayWithOffset = dayInMonth + offset;
+
+    // See https://stackoverflow.com/a/315767
+    const numDaysInCurrentMonth = AssignmentCenter.#numDaysInMonth(
+      date.getMonth(),
+      date.getFullYear(),
+    );
+    if (dayWithOffset > numDaysInCurrentMonth) {
+      // roll over to next month
+
+      const remainingOffset = dayWithOffset - numDaysInCurrentMonth;
+
+      // handle rolling over years too
+      // remember that months are 0-indexed (so 0-11)
+      const newYear = date.getFullYear() + (date.getMonth() === 11 ? 1 : 0);
+      const newMonth = (date.getMonth() + 1) % 12;
+      const newDay = 1;
+      const newDate = new Date(newYear, newMonth, newDay);
+
+      // be recursive to handle rolling over multiple months
+      return AssignmentCenter.#offsetFromDay(newDate, remainingOffset);
+    } else if (dayWithOffset < 1) {
+      // roll over to previous month
+
+      // handle rolling over years too
+      // remember that months are 0-indexed (so 0-11)
+      const newYear = date.getFullYear() - (date.getMonth() === 0 ? 1 : 0);
+      // `%` in js isn't modulo, it's remainder. https://stackoverflow.com/a/4467559
+      // This does modulo, so eg -2 -> 10
+      const newMonth = (((date.getMonth() - 1) % 12) + 12) % 12;
+
+      const numDaysInPrevMonth = AssignmentCenter.#numDaysInMonth(
+        newMonth,
+        newYear,
+      );
+
+      const newDay = numDaysInPrevMonth;
+      /** the end on the month */
+      const newDate = new Date(newYear, newMonth, newDay);
+
+      // dayWithOffset is negative or 0
+      // this is fine because this is setting the newDate to be the end of the month
+      const remainingOffset = dayWithOffset;
+
+      // be recursive to handle rolling over multiple months
+      return AssignmentCenter.#offsetFromDay(newDate, remainingOffset);
+    } else {
+      // oh my god finally the day is within the current month
+      // 1 <= dayWithOffset <= numDaysInCurrentMonth
+      const year = date.getFullYear();
+      const month = date.getMonth();
+      const day = dayWithOffset;
+
+      return new Date(year, month, day);
+    }
+  }
+
+  /**
+   * Get the number of days in a given month.
+   * @param {Number} month from 0-11 (Jan-Dec)
+   * @param {Number} year defaults to the current year
+   * @link https://stackoverflow.com/a/315767
+   */
+  static #numDaysInMonth(month, year = new Date().getFullYear()) {
+    // setting the day to 0 gets the last day of the previous month
+    return new Date(year, month + 1, 0).getDate();
+  }
+
+  /**
+   * Checks if two dates are on the same day.
+   * @param {Date} a
+   * @param {Date} b
+   */
+  static #datesAreSameDay(a, b) {
+    // Erase the time component from both, then compare.
+    const aErased = new Date(a.getFullYear(), a.getMonth(), a.getDate());
+    const bErased = new Date(b.getFullYear(), b.getMonth(), b.getDate());
+
+    return aErased.getTime() === bErased.getTime();
   }
 }
 
