2cbe691c909184633a51f303f14649e356fa2c2f
gavinmorrow <gavinfmorrow@gmail.com>
2024-10-14 10:51:09 -0400

    Add warning to description of custom task

--- src/content-scripts/assignment-center/AssignmentPopup.js
+++ src/content-scripts/assignment-center/AssignmentPopup.js
@@ -80,6 +80,10 @@ class AssignmentPopup extends HTMLElement {
   }
 
   #getDesc() {
+    // TODO: support custom tasks
+    if (this.assignment.details.isTask)
+      return "<i><strong>WARNING: CUSTOM TASKS ARE NOT YET SUPPORTED!!</strong> If you want support, email <a href='mailto:gavinmorrow@hunterschools.org'>gavinmorrow@hunterschools.org</a></i>";
+
     const rawDesc = this.assignment.description;
     if (rawDesc === null || rawDesc === undefined) return "<i>Loading...</i>";
     else if (rawDesc === "") return "<i>No description</i>";
