116e16ccb4e417c2bd7f828d74ddf2210dbcf00a
Gavin Morrow <gavinfmorrow@gmail.com>
2025-05-06 19:51:09 -0400

    Make error reporting use banner

--- src/util/report-error.js
+++ src/util/report-error.js
@@ -5,7 +5,25 @@
 const reportError = (err) => {
   console.error(err);
   if (shouldAlert(err)) {
-    alert(`Error: ${err.message}. If this persists, please contact Gavin.`);
+    // TODO: include count of how many times it occurred
+    const banner = BannerAlert.createBanner(
+      `Error: ${err.message}. If this persists, please contact Gavin.`,
+      "warning",
+      [
+        { name: "full-error", displayText: "Show full error" },
+        { name: "email", displayText: "Email Gavin" },
+      ],
+    );
+    const fullMessage = `${err}\n\nCause:\n${err.cause}\n\nStack:\n${err.stack}`;
+    banner.addEventListener("banner-alert-action-full-error", () => {
+      alert(fullMessage);
+    });
+    banner.addEventListener("banner-alert-action-email", () => {
+      const a = document.createElement("a");
+      a.href = `mailto:gavinmorrow${"@"}hunterschools${"."}org?subject=${encodeURI("Error in Hunter Extension")}&body=${encodeURI(fullMessage)}`;
+      a.target = "_blank";
+      a.click();
+    });
   }
 };
 
