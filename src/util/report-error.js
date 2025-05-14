9b208f6d3880d6fbde82ff115a270ed1925a7771
Gavin Morrow <gavinfmorrow@gmail.com>
2025-05-14 18:24:55 -0400

    Allow reportError to handle non-`ApiError`s

--- src/util/report-error.js
+++ src/util/report-error.js
@@ -32,13 +32,14 @@ const reportError = (err) => {
 let recentErrors = new Map();
 /** @param {ApiError} err @returns {bool}*/
 const shouldAlert = (err) => {
-  const numRecent = recentErrors.get(err.action) ?? 0;
+  const action = err.action ?? err.message;
+  const numRecent = recentErrors.get(action) ?? 0;
 
   // Increment count
-  recentErrors.set(err.action, numRecent + 1);
+  recentErrors.set(action, numRecent + 1);
   // Decrement count after period of time
   setTimeout(
-    () => recentErrors.set(err.action, recentErrors.get(err.action) - 1),
+    () => recentErrors.set(action, recentErrors.get(action) - 1),
     1 * 1000,
   );
 
