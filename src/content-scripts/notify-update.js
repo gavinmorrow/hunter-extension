5e546159470f1bf6b858e38a27b8e4871c63a760
Gavin Morrow <gavinfmorrow@gmail.com>
2025-05-14 18:56:45 -0400

    Use promiseError handler instead of reimpl in notify error

--- src/content-scripts/notify-update.js
+++ src/content-scripts/notify-update.js
@@ -50,41 +50,37 @@ promiseError(async () => {
   // Don't use Promise methods to avoid `InternalError: Promise rejection
   // value is a non-unwrappable cross-compartment wrapper.`
   // (see <https://bugzilla.mozilla.org/show_bug.cgi?id=1871516>)
-  try {
-    const latest = await getLatestVersion();
-    const newVersionAvailable = compareVersions(VERSION, latest) === 1;
-    const updateIgnored = (await getIgnoredUpdates()).has(latest);
+  const latest = await getLatestVersion();
+  const newVersionAvailable = compareVersions(VERSION, latest) === 1;
+  const updateIgnored = (await getIgnoredUpdates()).has(latest);
 
-    if (newVersionAvailable && !updateIgnored) {
-      const banner = BannerAlert.createBanner(
-        `New version available! Current: ${VERSION}, Latest: ${latest}`,
-        "info",
-        [
-          { name: "show-update", displayText: "How to update" },
-          { name: "remind-later", displayText: "Remind me later" },
-        ],
-        // TODO: include info on how to update
-      );
+  if (newVersionAvailable && !updateIgnored) {
+    const banner = BannerAlert.createBanner(
+      `New version available! Current: ${VERSION}, Latest: ${latest}`,
+      "info",
+      [
+        { name: "show-update", displayText: "How to update" },
+        { name: "remind-later", displayText: "Remind me later" },
+      ],
+      // TODO: include info on how to update
+    );
 
-      let ignoreUpdate = true;
-      banner.addEventListener("banner-alert-action-show-update", async () => {
-        ignoreUpdate = false;
-        banner.close();
+    let ignoreUpdate = true;
+    banner.addEventListener("banner-alert-action-show-update", async () => {
+      ignoreUpdate = false;
+      banner.close();
 
-        const a = document.createElement("a");
-        a.href = "https://gavinmorrow.github.io/orion/#updating";
-        // *Not* target=_blank, b/c updating works best when the extension isn't open.
-        a.click();
-      });
-      banner.addEventListener("banner-alert-action-remind-later", () => {
-        ignoreUpdate = false;
-        banner.close();
-      });
-      banner.addEventListener("banner-alert-close", () => {
-        if (ignoreUpdate) setIngoredUpdate(latest);
-      });
-    }
-  } catch (err) {
-    reportError(err);
+      const a = document.createElement("a");
+      a.href = "https://gavinmorrow.github.io/orion/#updating";
+      // *Not* target=_blank, b/c updating works best when the extension isn't open.
+      a.click();
+    });
+    banner.addEventListener("banner-alert-action-remind-later", () => {
+      ignoreUpdate = false;
+      banner.close();
+    });
+    banner.addEventListener("banner-alert-close", () => {
+      if (ignoreUpdate) setIngoredUpdate(latest);
+    });
   }
-})();
+}, reportError)();
