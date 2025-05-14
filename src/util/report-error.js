b43a2ab71255da28cefc125be758fac103210f36
Gavin Morrow <gavinfmorrow@gmail.com>
2025-05-14 18:27:21 -0400

    Handle errors thrown while reporting errors

--- src/util/report-error.js
+++ src/util/report-error.js
@@ -4,26 +4,42 @@
  */
 const reportError = (err) => {
   console.error(err);
-  if (shouldAlert(err)) {
-    // TODO: include count of how many times it occurred
-    const banner = BannerAlert.createBanner(
-      `Error: ${err.message}. If this persists, please contact Gavin.`,
-      "warning",
-      [
-        { name: "full-error", displayText: "Show full error" },
-        { name: "email", displayText: "Email Gavin" },
-      ],
-    );
-    const fullMessage = `${err}\n\nCause:\n${err.cause}\n\nStack:\n${err.stack}`;
-    banner.addEventListener("banner-alert-action-full-error", () => {
-      alert(fullMessage);
-    });
-    banner.addEventListener("banner-alert-action-email", () => {
-      const a = document.createElement("a");
-      a.href = `mailto:gavinmorrow${"@"}hunterschools${"."}org?subject=${encodeURI("Error in Orion")}&body=${encodeURI(fullMessage)}`;
-      a.target = "_blank";
-      a.click();
-    });
+
+  try {
+    if (shouldAlert(err)) {
+      // TODO: include count of how many times it occurred
+      const banner = BannerAlert.createBanner(
+        `Error: ${err.message}. If this persists, please contact Gavin.`,
+        "warning",
+        [
+          { name: "full-error", displayText: "Show full error" },
+          { name: "email", displayText: "Email Gavin" },
+        ],
+      );
+      const fullMessage = `${err}\n\nCause:\n${err.cause}\n\nStack:\n${err.stack}`;
+      banner.addEventListener("banner-alert-action-full-error", () => {
+        alert(fullMessage);
+      });
+      banner.addEventListener("banner-alert-action-email", () => {
+        const a = document.createElement("a");
+        a.href = `mailto:gavinmorrow${"@"}hunterschools${"."}org?subject=${encodeURI("Error in Orion")}&body=${encodeURI(fullMessage)}`;
+        a.target = "_blank";
+        a.click();
+      });
+    }
+  } catch (e) {
+    let msg = `Error thrown while reporting error:
+
+New Error: ${e}
+Cause:${e.cause}
+Stack:${e.stack}
+
+Original Error: ${err}
+Cause: ${err.cause}
+Stack: ${err.stack}`;
+
+    console.error(msg);
+    alert(msg);
   }
 };
 
